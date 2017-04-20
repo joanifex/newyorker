d7e0a8d261698da27d3bb0eb41b27d76d0890de0
Ian Wright <wright.ianb@gmail.com>
2017-04-20 09:25:56 -0600

    refactors and adds skeleton/normalize

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './styles/normalize.css';
+import './styles/skeleton.css';
+import './styles/main.css';
 
 ReactDOM.render(
   <App />,
