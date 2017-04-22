d152a75ebf16c6e955420ad4c26b86106f768e72
Ian Wright <wright.ianb@gmail.com>
2017-04-22 11:48:19 -0600

    adds hover pointer and onClick for Card

--- src/components/Cartoon.js
+++ src/components/Cartoon.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Cartoon = ({ cartoon, nextCartoon }) => (
-  <div className="card">
-    <div className="card-image" onClick={nextCartoon} >
+  <div className="card" onClick={nextCartoon}>
+    <div className="card-image">
       <img src={cartoon} className="cartoon-image" alt="New Yorker Cartoon"/>
     </div>
     <div className="card-text">
